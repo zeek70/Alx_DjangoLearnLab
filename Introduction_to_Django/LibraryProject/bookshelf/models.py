153bbf8cc64bd9c48ef5665fea3f00a14a8ae249
zeek70 <zaxbaaba@gmail.com>
2025-07-13 16:40:25 +0100

    commit

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Book(models.Model):
+    title=models.CharField(max_length=200)
+    author=models.CharField(max_length=100)
+    publication_year=models.IntegerField()
