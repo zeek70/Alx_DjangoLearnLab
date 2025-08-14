1449e6d70530c8445b093313fcf5857f146fc93b
zeek70 <zaxbaaba@gmail.com>
2025-08-14 14:56:22 +0100

    updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+def home_view(request):
+    return render(request,'blog/home.html')
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form .is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('profile')
+    else:
+        form=UserCreationForm()
+        return render(request,'registeration/profile.html',{'form':form})
+
+@login_required
+def profile_view(request):
+
+    return render(request, 'registration/profile.html', {'user': request.user})
+
+
 
 # Create your views here.
