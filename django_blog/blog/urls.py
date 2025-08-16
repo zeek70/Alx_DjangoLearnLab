2b1a338850b966be034b509838a33341dc05b04c
zeek70 <zaxbaaba@gmail.com>
2025-08-16 08:47:43 +0100

    dashboard created

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('',views.home_view,name='home'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('register/',views.register_view, name='register'),
